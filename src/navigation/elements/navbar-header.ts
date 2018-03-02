0ae0df4fe14f8013e9dc423b79938a561a406ac9
Alberto Arias <alberto@boodland.com>
2018-03-02 12:50:27 +0100

    Add button drawer hack in order to be able to apply app layout Css framework creates drawer button dynamicaly

--- src/navigation/elements/navbar-header.ts
+++ src/navigation/elements/navbar-header.ts
@@ -1,3 +1,9 @@
 export class NavbarHeader {
   title: string = 'Subject Hub';
+  drawerHandler() {
+    const drawerButton = document.getElementsByClassName('mdl-layout__drawer-button')[0] as HTMLElement;
+    if (drawerButton) {
+      drawerButton.click();
+    }
+  }
 }
