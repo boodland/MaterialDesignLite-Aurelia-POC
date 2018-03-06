707aa55b3e6533a982b1aeeaaac26cafc14dd7ef
Alberto Arias <alberto@boodland.com>
2018-03-06 18:37:35 +0100

    Add dashboard-item model

---
+++ src/dashboard/dashboard-item-model.ts
@@ -0,0 +1,9 @@
+export interface IDashboardItem {
+  subjectId: number;
+  title: string;
+  assessmentDate: Date;
+  numOfSeats: number;
+  numOfTutors: number;
+  numOfExams: number;
+  image: string;
+}
