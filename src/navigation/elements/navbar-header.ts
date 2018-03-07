27b59759cc52c82436feb531543c2de896b25f2a
Alberto Arias <alberto@boodland.com>
2018-03-07 13:38:38 +0100

    Show apps navbar header button if dashboard is the current route

--- src/navigation/elements/navbar-header.ts
+++ src/navigation/elements/navbar-header.ts
@@ -1,6 +1,11 @@
+import { bindable } from 'aurelia-framework';
+
 export class NavbarHeader {
+
   title: string = 'Subject Hub';
-  drawerHandler() {
+  @bindable isDashboardCurrentRoute: boolean;
+
+  drawerHandler(): void {
     const drawerButton = document.getElementsByClassName('mdl-layout__drawer-button')[0] as HTMLElement;
     if (drawerButton) {
       drawerButton.click();
