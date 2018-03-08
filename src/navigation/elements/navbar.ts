a1c59994377ed15e02e2105f54376094c4542a92
Alberto Arias <alberto@boodland.com>
2018-03-08 14:12:28 +0100

    Subscribe to router:navigation:success to update current route flag

--- src/navigation/elements/navbar.ts
+++ src/navigation/elements/navbar.ts
@@ -1,15 +1,34 @@
-import { Router, NavModel } from 'aurelia-router';
-import { bindable } from 'aurelia-framework';
+import { Router, NavModel, RouteConfig, NavigationInstruction } from 'aurelia-router';
+import { bindable, autoinject } from 'aurelia-framework';
+import { EventAggregator, Subscription } from 'aurelia-event-aggregator';
 
+@autoinject
 export class Navbar {
   
   @bindable router: Router;
   isDashboardCurrentRoute: boolean;
   navigation: NavModel[];
+  ea: EventAggregator;
+  sub: Subscription;
+
+  constructor(eventAggregator: EventAggregator) {
+    this.ea = eventAggregator;
+  }
 
   attached(): void {
-    this.isDashboardCurrentRoute = this.router.currentInstruction.config.title === 'dashboard';
+    this.updateDashboardCurrentRouteFlag(this.router.currentInstruction.config.title);
     this.navigation = this.router.navigation;
+    this.sub = this.ea.subscribe('router:navigation:success', (response) => {
+      this.updateDashboardCurrentRouteFlag(response.instruction.config.title);
+    });
+  }
+
+  detached() {
+    this.sub.dispose();
+  }
+
+  updateDashboardCurrentRouteFlag(routeTitle: string): void {
+    this.isDashboardCurrentRoute = routeTitle === 'Dashboard';
   }
 
   drawerToggleHandler(): void {
