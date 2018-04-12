61349096c728a7e1806bdbebfc54b1c774183a54
Alberto Arias <alberto@boodland.com>
2018-04-12 22:41:48 +0200

    Remove spaces within class

--- src/navigation/elements/navbar-header.ts
+++ src/navigation/elements/navbar-header.ts
@@ -1,11 +1,9 @@
 import { bindable } from 'aurelia-framework';
 
 export class NavbarHeader {
-
   title: string = 'Subject Hub';
   @bindable hideMainPageAction: boolean;
   @bindable menuButtonClickHandler: Function;
   @bindable dashboardButtonClickHandler: Function;
   @bindable backButtonClickHandler: Function;
-
 }
