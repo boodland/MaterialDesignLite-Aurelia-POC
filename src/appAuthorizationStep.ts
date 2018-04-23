fe9563ea61f94756ae7a92e18f27be285a7633b5
Alberto Arias <alberto@boodland.com>
2018-04-23 10:46:57 +0200

    Add authorization step to restrict access to protected resources

---
+++ src/appAuthorizationStep.ts
@@ -0,0 +1,11 @@
+import { NavigationInstruction, Next } from 'aurelia-router';
+
+export class AppAuthorizationStep {
+  run(navigationInstruction: NavigationInstruction, next: Next): Promise<any> {
+    if (navigationInstruction.getAllInstructions().some(i => i.config.settings.requiresAuth)) {
+      return next.cancel();  
+    }
+
+    return next();
+  }
+}
