2319a3cc96fe09cb980e5e9427ea7c0520fdb619
Alberto Arias <alberto@boodland.com>
2018-04-24 12:18:31 +0200

    Remove aurelia-dialog as it doesn't work straight away with router steps

--- src/app-authorization-step.ts
+++ src/app-authorization-step.ts
@@ -1,18 +1,13 @@
 import { NavigationInstruction, Next } from 'aurelia-router';
-import { DialogService } from 'aurelia-dialog';
-import { inject } from 'aurelia-framework';
 
-@inject(DialogService)
 export class AppAuthorizationStep {
   
-  constructor(private dialogService: DialogService) {
-  }
-
   run(navigationInstruction: NavigationInstruction, next: Next): Promise<any> {
     if (navigationInstruction.getAllInstructions().some(i => i.config.settings.requiresAuth)) { 
-      this.dialogService.open().whenClosed(response => { return next.cancel(); })
+      return next.cancel();
     }
-
-    return next();
+    else {
+      return next();
+    }
   }
 }
